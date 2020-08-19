6d683fb58732a1daec2324175528ec4c4f66f8ba
Chirag Mhamunkar <chirag.mhamunkar@turtlemint.com>
2020-08-19 18:27:03 +0530

    Remove unnecessary column from permission table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS auth_user (id SERIAL PRIMARY KEY, client VARCHAR(255), userId VARCHAR(255), tenant VARCHAR(255), createdAt timestamp, updatedAt timestamp);
 CREATE TABLE IF NOT EXISTS auth_role (id SERIAL PRIMARY KEY, key VARCHAR(255), name VARCHAR(255), tenant VARCHAR(255), active BOOLEAN,createdAt timestamp, updatedAt timestamp);
 
-CREATE TABLE IF NOT EXISTS auth_permission (id SERIAL PRIMARY KEY, key VARCHAR(255), name VARCHAR(255), tenant VARCHAR(255), active BOOLEAN,createdAt timestamp, updatedAt timestamp);
+CREATE TABLE IF NOT EXISTS auth_permission (id SERIAL PRIMARY KEY, key VARCHAR(255), name VARCHAR(255), active BOOLEAN,createdAt timestamp, updatedAt timestamp);
